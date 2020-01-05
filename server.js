f58101c9122ebbb2d471ac5e5a7c117a837dda5d
senthilbalajiganesan <senthilbalaji.ganesan>
2020-01-05 21:16:22 +0530

    added dockerfile and fixed api and ui hosting

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const http = require("http");
 const app = express();
+const compression = require("compression");
+const path = require("path");
 const YOUTUBE_META_URL = "http://www.youtube.com/oembed?format=json&url=";
+
+app.use(compression());
 
 app.get("/api/youtubeMeta", async (req, res) => {
   http.get(`${YOUTUBE_META_URL}${req.query.url}`, youtubeMetaRes => {
@@ -12,4 +16,12 @@ app.get("/api/youtubeMeta", async (req, res) => {
   });
 });
 
-app.listen(3001);
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, "/build")));
+
+// Handles any requests that don't match the ones above
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+
+app.listen(1234);
